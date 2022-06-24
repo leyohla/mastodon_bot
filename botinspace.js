8ea9fa601f178edd1172f0b9619b26a99f3479f9
Leyla Hunn <leyla.hunn@outlook.com>
2022-06-24 13:20:49 +0100

    credential changes

--- botinspace.js
+++ botinspace.js
@@ -1,11 +1,14 @@
+//Send POST request 
+
+require('dotenv').config();
 const Mastodon = require('mastodon-api');
 
 console.log("Mastodon Bot starting...");
 
 const M = new Mastodon({
-    client_key: '8niweoTfOF_umD6nRKHphew3R5VhThXnOq31I9ZYyhM',
-    client_secret: 'RyTHGcRxDsV16sKRCqlBYJqaOUTCyqGr4OnkduSqJLc',
-    access_token: 'AVc4G-rPbl6ebM53P7pviyDWKmTxJCEGm2rQQpBYWa0',
+    client_key: process.env.CLIENT_KEY,
+    client_secret: process.env.CLIENT_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
     timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
     api_url: 'https://botsin.space/api/v1/', // optional, defaults to https://mastodon.social/api/v1/
   })
